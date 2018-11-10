891b47e30d44ffeeae4f3401ac3256d635c35057
fosterwrecker <fosterwrecker@gmail.com>
2018-11-10 16:17:54 -0600

    attempt to get other files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function toggleNavbar() {
+
+	var nav = document.getElementById('navbar');
+
+	if (nav.style.display == '') {
+		nav.style.display = 'block';
+	} else {
+		nav.style.display = '';
+	}
+}
