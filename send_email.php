9aeeff7f27f49bd0386a01e305c06e95a2b89e90
fosterwrecker <44932624+fosterwrecker@users.noreply.github.com>
2023-05-31 16:28:42 -0500

    Add files via upload

--- send_email.php
+++ send_email.php
@@ -1,21 +1,27 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-  // Validate form inputs (you can add more validation if needed)
+    // Set up the email details
+    $to = 'fosterwrecker@gmail.com';  
+    $subject = 'New Contact Form Submission';
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-  // Forward the email
-  $to = 'dispatch@fosterwrecker.com';
-  $subject = 'Contact Form Submission';
-  $body = "Name: $name\nEmail: $email\n\n$message";
-  $headers = "From: $email";
+    // Compose the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message";
 
-  if (mail($to, $subject, $body, $headers)) {
-    echo 'Thank you for your message. We will get back to you soon.';
-  } else {
-    echo 'Sorry, there was an error sending your message. Please try again.';
-  }
+    // Send the email
+    if (mail($to, $subject, $email_content, $headers)) {
+        // Email sent successfully
+        echo "Thank you for your message. We will get back to you soon!";
+    } else {
+        // Error sending email
+        echo "Oops! An error occurred while sending your message. Please try again.";
+    }
 }
 ?>
